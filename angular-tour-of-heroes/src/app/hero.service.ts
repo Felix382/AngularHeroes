8a100af22ee420efaa7a04526e30c77876818a6f
Felix Facklam <facklam@meteolytix.de>
2019-11-07 22:39:54 +0100

    Implemented Logic for creation of messages

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './model/hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of} from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ export class HeroService {
       
 
     getHeroes(): Observable<Hero[]> {
+        this.messageService.add('HeroService: fetched Heros:');
         return of(HEROES);
     }
-    constructor() { }
+    constructor(private messageService: MessageService) { }
 }
